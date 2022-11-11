c304489bdde1cc75673c84f43404c8f71ed9d809
Prohor <prohor-yakovlev@mail.ru>
2022-11-11 17:54:55 +0300

    OneTask

---
+++ src/site/Body.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+type BodyType = {
+    titleForBody: string
+}
+
+const Body = (props:BodyType) => {
+    return (
+        <div>{props.titleForBody}</div>
+    )
+}
+
+export default Body
---
+++ src/site/Header.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+type HeaderType = {
+    title: string
+}
+
+const Header = (props: HeaderType) => {
+    return (
+        <>{props.title}</>
+    )
+}
+
+export default Header
