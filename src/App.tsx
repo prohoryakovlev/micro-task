52f521864b2c3734c051102f5ad6ea6c0b694f63
Prohor <prohor-yakovlev@mail.ru>
2022-11-11 18:25:39 +0300

    Add Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, {useState} from 'react';
+import NewComponent from "./site/NewComponent";
 import logo from './logo.svg';
 import './App.css';
 import Header from "./site/Header";
@@ -7,14 +8,27 @@ import Footer from "./site/Footer";
 
 
 
+
+
+
 function App() {
-  return (
-    <div className="App">
-        <Header title={"NEW BODY"}/>
-        <Body titleForBody={"NEW BODY1"}/>
-        <Footer/>
-    </div>
-  );
+    const [students, setStudents] = useState([
+            {id: 1, name: "James", age: 8},
+            {id: 2, name: "Robert", age: 18},
+            {id: 3, name: "John", age: 28},
+            {id: 4, name: "Michael", age: 38},
+            {id: 5, name: "William", age: 48},
+            {id: 6, name: "David", age: 58},
+            {id: 7, name: "Richard", age: 68},
+            {id: 8, name: "Joseph", age: 78},
+            {id: 9, name: "Thomas", age: 88},
+            {id: 10, name: "Charles", age: 98},
+            {id: 11, name: "Christopher", age: 100},
+        ]
+    )
+    return (
+        <NewComponent students={"student"}/>
+    );
 }
 
 export default App;
---
+++ src/site/NewComponent.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import App from "../App";
+
+type NewComponent = {
+    students: any
+}
+
+
+
+const NewComponent = (props: NewComponent) => {
+    return (
+        <div>NewComponent</div>
+    )
+}
+
+export default NewComponent
